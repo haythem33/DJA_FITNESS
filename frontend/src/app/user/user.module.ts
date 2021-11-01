5ca5df48f7efc34a1c74a2ae96224554b7d1f5c4
haythem33 <haythembenjbara@gmail.com>
2021-11-01 20:05:58 +0100

    frontend architecture

---
+++ frontend/src/app/user/user.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SubscribeComponent } from './subscribe/subscribe.component';
+
+
+
+@NgModule({
+  declarations: [
+    SubscribeComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class UserModule { }
